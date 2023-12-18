43460ee744dacba19df052a0ee7d7942c92dac39
Ariefdarma0522 <ariefdarmawan468@gmail.com>
2023-12-18 22:56:11 +0700

    first commit

---
+++ latihan-volume-bangun-ruang/latihan-persiapan-ujian1-volume-balok/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace latihan_persiapan_ujian1_volume_balok
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Buatlah aplikasi untuk menghitung Volume / isi
+
+            Console.WriteLine("Pilih bidang 3D berikut : ");
+            Console.WriteLine("1. Balok");
+            Console.WriteLine("2. Kubus");
+            Console.WriteLine("3. Tabung");
+            Console.WriteLine("4. Bola");
+            Console.WriteLine("5. Kerucut");
+
+            string nama = null;
+            char ulang;
+            double ukuran, panjang, lebar,jari2, tinggi, hasil;
+
+
+            do
+            {
+                Console.Write("Pilih bangun 3D yang akan dihitung: ");
+                ukuran = int.Parse(Console.ReadLine());
+
+            
+                switch (ukuran) // Untuk memilih bagung ruang apa yang akan dihitung
+                {
+                    case 1:
+                        // Untuk menampilkan nama dari bangun ruang
+                        nama = "Balok";
+                        Console.WriteLine(nama);
+                        // Untuk memasukkan nilai panjang
+                        Console.Write("Masukkan panjang : ");
+                        panjang = int.Parse(Console.ReadLine());
+                        // Untuk memasukkan nilai lebar
+                        Console.Write("Masukkan Lebar : ");
+                        lebar = int.Parse(Console.ReadLine());
+                        // Untuk memasukkan nilai tinggi
+                        Console.Write("Masukkan Tinggi : ");
+                        tinggi = int.Parse(Console.ReadLine());
+                        // Untuk mencari hasil & manampilkan hasil dari volume
+                        hasil = panjang * lebar * tinggi;
+                        Console.WriteLine($"Volume Balok = {hasil} cm3");
+                        break;
+                    case 2:
+                        // Untuk menampilkan nama dari bangun ruang
+                        nama = "Kubus";
+                        Console.WriteLine(nama);
+                        // Untuk memasukkan nilai sisi dari kubus
+                        Console.Write("masukkan ukuran sisinya : ");
+                        panjang = int.Parse(Console.ReadLine());
+                        // Untuk mencari hasil & menampilkan hasil dari kubus
+                        hasil = Math.Pow(panjang, 3);
+                        Console.WriteLine($"Volume Kubus = {hasil} cm3");
+                        break;
+                    case 3:
+                        // Untuk menampilkan nama dari bangun ruang
+                        nama = "Tabung";
+                        Console.WriteLine(nama);
+                        // Untuk memasukkan nilai jari-jari dari tabung
+                        Console.Write("Masukkan Jari-jari dari tabung : ");
+                        jari2 = int.Parse(Console.ReadLine());
+                        // Untuk memasukkan nalai tinggi dari tabung
+                        Console.Write("Masukkan tinggi dari kerucut : ");
+                        tinggi = int.Parse(Console.ReadLine());
+                        // Untuk mencari hasil & menampilkan hasil
+                        hasil = Math.PI * Math.Pow(jari2, 2) * tinggi;
+                        Console.WriteLine($"Volume dari Tabung = {hasil} cm3");
+                        break;
+                    case 4:
+                        // Untuk menampilkan nama dari bangun ruang
+                        nama = "Bola";
+                        Console.WriteLine(nama);
+                        // Untuk memasukkan nilai dari jari-jari bola
+                        Console.Write("Masukkan Jari-jari dari bola : ");
+                        jari2 = int.Parse(Console.ReadLine());
+                        // Untuk mencari hasil & menampilkan hasil dari bangun ruang bola
+                        hasil = (4.0 /3.0) * Math.PI * Math.Pow(jari2, 3);
+                        Console.WriteLine($"Volume dari bola = {hasil} cm3");
+                        break;
+                    case 5:
+                        // Untuk menampilkan nama dari bangun ruang
+                        nama = "Kerucut";
+                        Console.WriteLine(nama);
+                        // Untuk memasukkan nilai jari2 dari kerucut
+                        Console.Write("masukkan jari-jari dari kerucut : ");
+                        jari2 = int.Parse(Console.ReadLine());
+                        // Untuk memasukkan nilai tinggi dari kerucut
+                        Console.Write("Masukkan tinggi dari kerucut : ");
+                        tinggi = int.Parse(Console.ReadLine());
+                        // Untuk mencari hasil dari kerucut & menampilkan hasil
+                        hasil = (1.0 / 3.0) * Math.PI * Math.Pow(jari2, 2) * tinggi;
+                        Console.WriteLine($"Volume dari kerucut = {hasil}");
+                        break;
+
+                }
+                Console.WriteLine("Apakah anda ingin megulangi lagi ? [Y/T]");
+                ulang = Convert.ToChar(Console.ReadLine());
+            } while ((ulang == 'Y') || (ulang == 'y'));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ latihang-luas-bangun-datar/latihan-p13-menghitung-luas-bangundatar/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace latihan_p13_menghitung_luas_bangundatar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Buatlah aplikasi untuk menghitung luas tanah / bangunan berbentuk
+            Console.WriteLine("APLIKASI LUAS TANAH : ");    
+
+            Console.WriteLine(" 1. Lingkaran");
+            Console.WriteLine(" 2. Segitiga");
+            Console.WriteLine(" 3. Trapesium");
+            Console.WriteLine(" 4. Persegi panjang");
+            Console.WriteLine(" 5. Jajargenjang");
+
+            string nama;
+            char ulang;
+            double jari2, panjang, lebar, tinggi, hasil;
+            int luasTanah;
+
+            do
+            {
+                Console.Write("Pilih Nomor = ");
+                luasTanah = int.Parse(Console.ReadLine());
+
+                switch (luasTanah)
+                {
+                    case 1:
+                        nama = "Lingkarang";
+                        Console.WriteLine(nama);
+                        Console.Write("Masukkan jari2 dari lingkarang = ");
+                        jari2 = int.Parse(Console.ReadLine());
+                        hasil = Math.PI * Math.Pow(jari2, 2);
+                        Console.Write($"Luas tanah lingkaran = {hasil} cm2");
+                        break;
+                    case 2:
+                        nama = "segitiga";
+                        Console.WriteLine(nama);
+                        Console.Write("Masukkan alas segitiga : ");
+                        panjang = int.Parse(Console.ReadLine());
+                        Console.Write("Masukkan tinggi dari segitiga : ");
+                        tinggi = int.Parse(Console.ReadLine());
+                        hasil = 0.5 * panjang * tinggi;
+                        Console.WriteLine($"Luas dari tanah segitiga : {hasil} cm2");
+                        break;
+                    case 3:
+                        nama = "Trapesium";
+                        Console.WriteLine(nama);
+                        Console.Write("Masukkan panjang sisi \"A\" : ");
+                        panjang = int.Parse(Console.ReadLine());
+                        Console.Write("Masukkan panjang sisi \"B\" : ");
+                        lebar = int.Parse(Console.ReadLine());
+                        Console.Write("Masukkan tinggi dari trapesium : ");
+                        tinggi = int.Parse(Console.ReadLine());
+                        hasil = 0.5 * tinggi * (panjang * lebar);
+                        Console.WriteLine($"Luas dari tanah trapesium : {hasil} cm2");
+                        break;
+                    case 4:
+                        nama = "persegi panjang";
+                        Console.WriteLine(nama);
+                        Console.Write("Masukkan panjang : ");
+                        panjang = int.Parse(Console.ReadLine());
+                        Console.Write("Masukkan lebar : ");
+                        lebar = int.Parse(Console.ReadLine());
+                        hasil = panjang * lebar;
+                        Console.WriteLine($"Luas tanah persegi panjang : {hasil} cm2");
+                        break;
+                    case 5:
+                        nama = "jajar genjang";
+                        Console.WriteLine(nama);
+                        Console.Write("Masukkan panjang alas : ");
+                        panjang = int.Parse(Console.ReadLine());
+                        Console.Write("Masukkan tinggi : ");
+                        tinggi = int.Parse(Console.ReadLine());
+                        hasil = panjang * tinggi;
+                        Console.WriteLine($"Luas tanah jajar genjang : {hasil} cm2");
+                        break;
+                }
+                Console.WriteLine("apakah anda ingin menghitung bentuk yang lain? [Y/T]");
+                ulang = Convert.ToChar(Console.ReadLine());
+            } while ((ulang == 'Y') || (ulang == 'y'));
+
+
+            Console.Read();
+        }
+    }
+}
