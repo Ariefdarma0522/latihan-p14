e23709d72914e3ff1c82b010c4eb3479338b43f7
ariefdrm <ariefdarmawan5522@gmail.com>
2024-01-06 12:09:45 +0700

    update kalkulator sederhana

--- new-kalkulator-sederhana/new-kalkulator-sederhana/Program.cs
+++ new-kalkulator-sederhana/new-kalkulator-sederhana/Program.cs
@@ -12,7 +12,6 @@ namespace new_kalkulator_sederhana
             // Masukkan operator (+,-,*,/): *
             // Hasilnya adalah : 50
 
-            // Menginisialisasi variabel
             float angka1, angka2, hasil = 0;
 
             // Untuk memasukkan angka ke 1
@@ -27,54 +26,35 @@ namespace new_kalkulator_sederhana
             Console.Write("Masukkan operator(+,-,*,/) : ");
             int pilihan = Convert.ToChar(Console.ReadLine());
 
-            // If, saya gunakan utk mengubah kembali variabel "Pilihan" dari 'char' menjadi 'int' kembali. Untuk menjalankan fungsi switch 
-            if (pilihan == '+')
+            switch (pilihan)
             {
-                pilihan = 1;
-            }
-            else if (pilihan == '-')
-            {
-                pilihan = 2;
-            }
-            else if (pilihan == '*')
-            {
-                pilihan = 3;
-            }
-            else if (pilihan == '/')
-            {
-                pilihan = 4;
-            }
+                case '+':
+                    hasil = angka1 + angka2;
+                    Console.Write($"Hasil : {hasil:N}");
+                    break;
+                case '-':
+                    hasil = angka1 - angka2;
+                    Console.Write($"Hasil : {hasil:N}");
+                    break;
+                case '*':
+                    hasil = angka1 * angka2;
+                    Console.Write($"Hasil : {hasil:N}");
+                    break;
+                case '/':
+                    if (angka2 != 0)
+                    {
+                        hasil = angka1 / angka2;
+                        Console.Write($"Hasil : {hasil:N}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error : angka tidak dapat dibagi dengan 0 (nol)");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Error : Masukkan Symbol operasi yang ada!!!");
+                    break;
 
-            // If, saya gunakan utk menghindari error ketika code dijalankan tetapi tidak menggunakan symbol yang ada, yang dimana jika code ttp dijalan console ttp akan menampilkan hasil walaupun hasil nya adalah 0 
-            if (pilihan < 5)
-            {
-                switch (pilihan)
-                {
-                    case 1:
-                        hasil = angka1 + angka2;
-                        break;
-                    case 2:
-                        hasil = angka1 - angka2;
-                        break;
-                    case 3:
-                        hasil = angka1 * angka2;
-                        break;
-                    case 4:
-                        if (angka2 != 0)
-                        {
-                            hasil = angka1 / angka2;
-                        }
-                        else
-                        {
-                            Console.WriteLine("Error : angka tidak dapat dibagi dengan 0 (nol)");
-                        }
-                        break;
-                }
-                Console.Write($"Hasil : {hasil:N}");
-            }
-            else
-            {
-                Console.WriteLine("Error : Masukkan symbol operasi yang ada!!!");
             }
 
             Console.ReadKey();
